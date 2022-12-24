ed59058677ea1afd56ebc2943cef593d5dd85ba4
Paul Wolanski <p.wolanski@kunzleigh.com>
2022-12-24 16:23:54 -0500

    nx a14

---
+++ apps/material-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'a14-sandbox-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'material-demo';
+}
